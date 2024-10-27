71ed87cc5be75567aa38414a04c9abc096e8882e
Tariq <tariq.m@zohocorp.com>
2024-10-27 15:50:27 +0530

    [DESIGN]: basic layout of application designed

--- src/Database/Database.java
+++ src/Database/Database.java
@@ -1,4 +1,41 @@
 package Database;
 
+import model.Message;
+import model.User;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+
 public class Database {
+
+    private static volatile Database databaseInstance = null;
+
+    private final CopyOnWriteArrayList<Message> messages = new CopyOnWriteArrayList<>();
+    private final CopyOnWriteArrayList<User> users = new CopyOnWriteArrayList<>();
+
+    static final Object LOCK = new Object();
+
+    private Database(){}
+
+    public static Database getDatabaseInstance(){
+        if(databaseInstance == null){
+            synchronized (LOCK){
+                if(databaseInstance == null){
+                    return databaseInstance = new Database();
+                }
+            }
+        }
+        return databaseInstance;
+    }
+
+    //Write operations
+    public void writeMessage(Message message){
+        messages.add(message);
+    }
+
+    public void writeUser(User user){
+        users.add(user);
+    }
+
+    //Read operations
+
 }
--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,4 +1,24 @@
 package controller;
 
+import Database.Database;
+
 public class Controller {
+
+    private static final Object LOCK = new Object();
+
+    private final Database database = Database.getDatabaseInstance();
+    private static volatile Controller controllerInstance;
+
+    private Controller(){}
+
+    public static Controller getControllerInstance(){
+        if(controllerInstance==null){
+            synchronized (LOCK){
+                if(controllerInstance==null){
+                    return controllerInstance = new Controller();
+                }
+            }
+        }
+        return controllerInstance;
+    }
 }
